07763ef0296270937e31635f5010c1774f28c25d
hustle2live <kuz.v@ukr.net>
2023-06-01 09:46:08 +0300

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
-
-function App() {
-   return (
-      <div className='App'>
-         <header className='App-header'>HELLO WORLD</header>
-      </div>
-   );
-}
+const App = () => (
+   <div className='App'>
+      <header className='App-header'>HELLO WORLD</header>
+   </div>
+);
 
 export default App;
