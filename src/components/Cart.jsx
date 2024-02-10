b1dfdb91b925e92f861887e8ca2047cec66f48d7
hustle2live <kuz.v@ukr.net>
2024-02-10 13:39:33 +0200

    moderate cart items styles

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -48,7 +48,7 @@ const ShoppingCart = () => {
          {/* <h2 className={styles.cart_heading}>SHOPPING CART PAGE </h2> */}
 
          <div
-            className={`${styles.cart_wrapper} bg-gray-50 border-gray-900/10 m-0 sm:m-8 md:m-14 lg:mx-32 pt-12 pb-12 border-2 rounded-md`}
+            className={`${styles.cart_wrapper} bg-gray-50 border-gray-900/10 m-0 sm:m-8 md:m-14 lg:mx-32 p-12 pl-0 border-2 rounded-md`}
          >
             <div className={styles.cart_wrapper__userSection}>
                <h2 className='ml-12 text-lg font-semibold leading-7 text-gray-900'>Contact information</h2>
@@ -56,7 +56,7 @@ const ShoppingCart = () => {
                   This information will be displayed publicly so be careful what you share.
                </p> */}
 
-               <form className='ml-12 mt-0 mb-12 p-0'>
+               <form className='m-12 mr-6 mt-0 p-0'>
                   <div className='mt-4'>
                      <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor='userEmail'>
                         Email address:
@@ -152,45 +152,87 @@ const ShoppingCart = () => {
                   </div>
                </form>
             </div>
-            <div className={styles.cart_wrapper__cartSection}>
-               {cartState.map(({ NAME, PRICE, IMG_SRC, QUANTITY }) => {
-                  const MenuImage = IMG_SRC ? require(`../${IMG_SRC}`) : Plate;
+            <div className=''>
+               <h2 className='ml-4 mb-4 text-lg font-semibold leading-7 text-gray-900'>Order summary:</h2>
 
-                  const itemPrice = Math.round(PRICE * QUANTITY * 100) / 100;
+               <div className='bg-white p-8 border-gray-300 border'>
+                  <div ul role='list' className='-my-6 divide-y divide-gray-200'>
+                     {cartState.map(({ NAME, PRICE, IMG_SRC, QUANTITY }) => {
+                        const MenuImage = IMG_SRC ? require(`../${IMG_SRC}`) : Plate;
 
-                  return (
-                     <div className={styles.shopMenu__item} key={NAME}>
-                        <img className={styles.shopMenu__item_image} src={MenuImage} alt={NAME} />
+                        const itemPrice = Math.round(PRICE * QUANTITY * 100) / 100;
 
-                        <div className={styles.shopMenu__item_frame}>
-                           <h4 className={styles.shopMenu__item_name}>{NAME}</h4>
+                        return (
+                           <li className='flex py-6' key={NAME}>
+                              <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+                                 <img src={MenuImage} alt={NAME} className='h-full w-full object-cover object-center' />
+                              </div>
 
-                           <div className={styles.shopMenu__item_price}>
-                              <p>
-                                 Price: <span>${PRICE}</span>
-                                 <span className={styles.shopMenu__item_price_total}>
-                                    Total: <span>${itemPrice}</span>
-                                 </span>
-                              </p>
-                           </div>
-                           <input
-                              type='number'
-                              min={0}
-                              max={10}
-                              value={QUANTITY}
-                              className={styles.cart_wrapper__cartSection_inputCount}
-                              onChange={(e) => handleCountChange(NAME, e.target.value)}
-                           />
-                           <button
-                              className={styles.cart_wrapper__cartSection_deleteButton}
-                              onClick={(e) => dispatch(dishRemoved(NAME))}
-                           >
-                              <span className='material-symbols-outlined'>close</span>
-                           </button>
-                        </div>
-                     </div>
-                  );
-               })}
+                              <div className='ml-4 flex flex-1 flex-col'>
+                                 <div>
+                                    <div className='flex justify-between text-base font-medium text-gray-900'>
+                                       <h3>{NAME}</h3>
+                                       <p className='ml-4'>${itemPrice}</p>
+                                    </div>
+                                    <p className='mt-1 text-sm text-gray-500'>
+                                       {' '}
+                                       Price: <span>${PRICE}</span>
+                                    </p>
+                                 </div>
+                                 <div className='flex flex-1 items-end justify-between text-sm'>
+                                    <p className='text-gray-500'>Quantity: </p>
+
+                                    <input
+                                       type='number'
+                                       min={0}
+                                       max={10}
+                                       value={QUANTITY}
+                                       className="border-gray-300 border-2 rounded-md pl-2"
+                                       onChange={(e) => handleCountChange(NAME, e.target.value)}
+                                    />
+
+                                    <div className='flex'>
+                                       <button
+                                          type='button'
+                                          className='font-medium text-indigo-600 hover:text-indigo-500'
+                                       >
+                                          Remove
+                                       </button>
+                                    </div>
+                                 </div>
+                              </div>
+
+                              {/* <div className={styles.shopMenu__item_frame}>
+                                 <h4 className={styles.shopMenu__item_name}>{NAME}</h4>
+
+                                 <div className={styles.shopMenu__item_price}>
+                                    <p>
+                                       Price: <span>${PRICE}</span>
+                                       <span className={styles.shopMenu__item_price_total}>
+                                          Total: <span>${itemPrice}</span>
+                                       </span>
+                                    </p>
+                                 </div>
+                                 <input
+                                    type='number'
+                                    min={0}
+                                    max={10}
+                                    value={QUANTITY}
+                                    className={styles.cart_wrapper__cartSection_inputCount}
+                                    onChange={(e) => handleCountChange(NAME, e.target.value)}
+                                 />
+                                 <button
+                                    className={styles.cart_wrapper__cartSection_deleteButton}
+                                    onClick={(e) => dispatch(dishRemoved(NAME))}
+                                 >
+                                    <span className='material-symbols-outlined'>close</span>
+                                 </button>
+                              </div> */}
+                           </li>
+                        );
+                     })}
+                  </div>
+               </div>
             </div>
             <div className={styles.cart_wrapper__totalSection}>
                <p className={styles.cart_wrapper__totalSection_totalAmount}>
