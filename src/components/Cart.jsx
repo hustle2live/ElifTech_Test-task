d120fd017f868273a643cb6e91186705f7cfaebd
hustle2live <kuz.v@ukr.net>
2024-02-11 12:10:05 +0200

    moderate cart section total amount

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { dishRemoved, setQuantity, getTotalPrice, orderRegister } from '../redux/cartReducer';
 
+import { XMarkIcon } from '@heroicons/react/20/solid';
+
 import Plate from '../images/plate.png';
 import styles from '../index.module.scss';
 
@@ -45,12 +47,10 @@ const ShoppingCart = () => {
 
    return (
       <div className='p-0'>
-         {/* <h2 className={styles.cart_heading}>SHOPPING CART PAGE </h2> */}
-
          <div
-            className={`${styles.cart_wrapper} bg-gray-50 border-gray-900/10 m-0 sm:m-8 md:m-14 lg:mx-32 p-12 pl-0 border-2 rounded-md`}
+            className={`${styles.cart_wrapper} relative bg-gray-50 border-gray-900/10 m-0 sm:m-8 md:m-14 lg:mx-32 p-12 pl-0 border-2 rounded-md`}
          >
-            <div className={styles.cart_wrapper__userSection}>
+            <div className={`${styles.cart_wrapper__userSection} w-1/2 block`}>
                <h2 className='ml-12 text-lg font-semibold leading-7 text-gray-900'>Contact information</h2>
                {/* <p className='ml-12 mt-1 text-sm leading-6 text-gray-600'>
                   This information will be displayed publicly so be careful what you share.
@@ -152,10 +152,10 @@ const ShoppingCart = () => {
                   </div>
                </form>
             </div>
-            <div className=''>
+            <div className='w-1/2 block'>
                <h2 className='ml-10 mb-6 text-lg font-semibold leading-7 text-gray-900'>Order summary:</h2>
 
-               <div className='ml-10 bg-white p-8 border-gray-200 border rounded-md min-w-80 min-h-24'>
+               <div className='ml-10 bg-white p-8 pr-14 border-gray-200 border rounded-md min-w-80 min-h-24 max-h-96 overflow-y-scroll'>
                   <ul className='-my-6 divide-y divide-gray-200 min-w-full'>
                      {cartState.map(({ NAME, PRICE, IMG_SRC, QUANTITY }) => {
                         const MenuImage = IMG_SRC ? require(`../${IMG_SRC}`) : Plate;
@@ -199,6 +199,7 @@ const ShoppingCart = () => {
                                        >
                                           Remove
                                        </button>
+                                       {/* <XMarkIcon className='h-5 w-5 text-gray-900' aria-hidden='true' /> */}
                                     </div>
                                  </div>
                               </div>
@@ -208,12 +209,59 @@ const ShoppingCart = () => {
                   </ul>
                </div>
             </div>
-            <div className={styles.cart_wrapper__totalSection}>
-               <p className={styles.cart_wrapper__totalSection_totalAmount}>
+            {/* <div className={styles.cart_wrapper__totalSection}> */}
+            <div className='absolute block box-border bottom-0 w-full'>
+               {/* <p className={styles.cart_wrapper__totalSection_totalAmount}>
                   <span>Total price : </span>
                   {cartTotal.toFixed(2)}
-               </p>
-               <input type='submit' value='Submit' onClick={formHandleSubmit} />
+               </p> */}
+               {/* <input type='submit' value='Submit' onClick={formHandleSubmit} /> */}
+
+               <div className='relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 p-4 px-32 sm:before:flex-1'>
+                  {/* blur background */}
+                  <div
+                     className='absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
+                     aria-hidden='true'
+                  >
+                     <div
+                        className='aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30'
+                        style={{
+                           clipPath:
+                              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
+                        }}
+                     />
+                  </div>
+
+                  <div
+                     className='absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
+                     aria-hidden='true'
+                  >
+                     <div
+                        className='aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30'
+                        style={{
+                           clipPath:
+                              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
+                        }}
+                     />
+                  </div>
+
+                  <div className='flex flex-wrap items-center gap-x-4 gap-y-2'>
+                     <p className='text-xl leading-6 text-gray-900'>
+                        <strong className='font-bold mr-12'>Total price :</strong>
+                        <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
+                           <circle cx={1} cy={1} r={1} />
+                        </svg>
+                        <span className='font-semibold'> $ {cartTotal.toFixed(2)}</span>
+                     </p>
+
+                     <input
+                        className='ml-14 flex-none rounded bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900'
+                        type='submit'
+                        value='Confirm Order'
+                        onClick={formHandleSubmit}
+                     />
+                  </div>
+               </div>
             </div>
          </div>
       </div>
