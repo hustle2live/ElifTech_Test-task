02d21b4c603bcae3e6ec72de4b7ad704b602bc01
hustle2live <kuz.v@ukr.net>
2024-02-11 15:19:35 +0200

    moderate cart items styles

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -46,16 +46,10 @@ const ShoppingCart = () => {
    }, [cartState, dispatch]);
 
    return (
-      <div className='p-0'>
-         <div
-            className={`${styles.cart_wrapper} relative bg-gray-50 border-gray-900/10 m-0 sm:m-8 md:m-14 lg:mx-32 p-12 pl-0 border-2 rounded-md`}
-         >
-            <div className={`${styles.cart_wrapper__userSection} w-1/2 block`}>
+      <div className={styles.cart_wrapper}>
+         <div className='flex flex-wrap flex-col-reverse lg:flex-row lg:flex-nowrap relative bg-gray-50 border-gray-900/10 m-0 sm:m-8 md:m-14 lg:mx-32 lg:gap-12 p-12 pl-0 border-2 rounded-md'>
+            <div className='w-full mt-12 lg:mt-0 lg:w-1/2 block'>
                <h2 className='ml-12 text-lg font-semibold leading-7 text-gray-900'>Contact information</h2>
-               {/* <p className='ml-12 mt-1 text-sm leading-6 text-gray-600'>
-                  This information will be displayed publicly so be careful what you share.
-               </p> */}
-
                <form className='m-12 mr-6 mt-0 p-0'>
                   <div className='mt-4'>
                      <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor='userEmail'>
@@ -152,109 +146,78 @@ const ShoppingCart = () => {
                   </div>
                </form>
             </div>
-            <div className='w-1/2 block'>
-               <h2 className='ml-10 mb-6 text-lg font-semibold leading-7 text-gray-900'>Order summary:</h2>
+            <div className='w-full pl-12 lg:pl-0 lg:w-1/2 2xl:w-1/3 block'>
+               <h2 className='mb-6 text-lg font-semibold leading-7 text-gray-900'>Order summary:</h2>
 
-               <div className='ml-10 bg-white p-8 pr-14 border-gray-200 border rounded-md min-w-80 min-h-24 max-h-96 overflow-y-scroll'>
-                  <ul className='-my-6 divide-y divide-gray-200 min-w-full'>
-                     {cartState.map(({ NAME, PRICE, IMG_SRC, QUANTITY }) => {
-                        const MenuImage = IMG_SRC ? require(`../${IMG_SRC}`) : Plate;
+               <ul className='bg-white p-8 pr-14 min-h-24 max-h-96 overflow-y-scroll divide-y divide-gray-200 min-w-full'>
+                  {cartState.map(({ NAME, PRICE, IMG_SRC, QUANTITY }) => {
+                     const MenuImage = IMG_SRC ? require(`../${IMG_SRC}`) : Plate;
 
-                        const itemPrice = (Math.round(PRICE * QUANTITY * 100) / 100).toFixed(2);
+                     const itemPrice = (Math.round(PRICE * QUANTITY * 100) / 100).toFixed(2);
 
-                        return (
-                           <li className='flex py-6' key={NAME}>
-                              <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
-                                 <img src={MenuImage} alt={NAME} className='h-full w-full object-cover object-center' />
-                              </div>
+                     return (
+                        <li className='flex py-6' key={NAME}>
+                           <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+                              <img src={MenuImage} alt={NAME} className='h-full w-full object-cover object-center' />
+                           </div>
 
-                              <div className='ml-4 flex flex-1 flex-col'>
-                                 <div>
-                                    <div className='flex justify-between text-base font-medium text-gray-900'>
-                                       <h3>{NAME}</h3>
-                                       <p className='ml-4'>${itemPrice}</p>
-                                    </div>
-                                    <p className='mt-1 text-sm text-gray-500'>
-                                       {' '}
-                                       Price: <span>${PRICE}</span>
-                                    </p>
+                           <div className='ml-4 flex flex-1 flex-col'>
+                              <div>
+                                 <div className='flex justify-between text-base font-medium text-gray-900'>
+                                    <h3>{NAME}</h3>
+                                    <p className='ml-4'>${itemPrice}</p>
                                  </div>
-                                 <div className='flex flex-1 items-end justify-between text-sm'>
-                                    <p className='text-gray-500'>Quantity: </p>
+                                 <p className='mt-1 text-sm text-gray-500'>
+                                    {' '}
+                                    Price: <span>${PRICE}</span>
+                                 </p>
+                              </div>
+                              <div className='flex flex-1 items-end justify-between text-sm'>
+                                 <p className='text-gray-500'>Quantity: </p>
 
-                                    <input
-                                       value={QUANTITY}
-                                       type='number'
-                                       min={0}
-                                       max={10}
-                                       className='border-gray-300 border-2 rounded-md pl-2'
-                                       onChange={(e) => handleCountChange(NAME, e.target.value)}
-                                    />
+                                 <input
+                                    value={QUANTITY}
+                                    type='number'
+                                    min={0}
+                                    max={10}
+                                    className='border-gray-300 border-2 rounded-md pl-2'
+                                    onChange={(e) => handleCountChange(NAME, e.target.value)}
+                                 />
 
-                                    <div className='flex'>
-                                       <button
-                                          type='button'
-                                          onClick={(e) => dispatch(dishRemoved(NAME))}
-                                          className='font-medium text-indigo-600 hover:text-indigo-500'
-                                       >
-                                          Remove
-                                       </button>
-                                       {/* <XMarkIcon className='h-5 w-5 text-gray-900' aria-hidden='true' /> */}
-                                    </div>
+                                 <div className='flex'>
+                                    <button
+                                       type='button'
+                                       onClick={(e) => dispatch(dishRemoved(NAME))}
+                                       className='font-medium text-indigo-600 hover:text-indigo-500'
+                                    >
+                                       Remove
+                                    </button>
+                                    {/* <XMarkIcon className='h-5 w-5 text-gray-900' aria-hidden='true' /> */}
                                  </div>
                               </div>
-                           </li>
-                        );
-                     })}
-                  </ul>
+                           </div>
+                        </li>
+                     );
+                  })}
+               </ul>
+
+               <div className='block box-block w-full relative isolate flex justify-evenly center p-8 pb-0 bg-white'>
+                  <p className='text-xl leading-6 text-gray-900'>
+                     <strong className='font-bold mr-2'>Total price :</strong>
+                     <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
+                        <circle cx={1} cy={1} r={1} />
+                     </svg>
+                     <span className='font-semibold'> $ {cartTotal.toFixed(2)}</span>
+                  </p>
                </div>
-            </div>
-            {/* <div className={styles.cart_wrapper__totalSection}> */}
-            <div className='absolute block box-border bottom-0 w-full'>
-               <div className='relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 p-4 px-32 sm:before:flex-1'>
-                  {/* blur background */}
-                  {/* <div
-                     className='absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
-                     aria-hidden='true'
-                  >
-                     <div
-                        className='aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30'
-                        style={{
-                           clipPath:
-                              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
-                        }}
-                     />
-                  </div> */}
 
-                  {/* <div
-                     className='absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
-                     aria-hidden='true'
-                  >
-                     <div
-                        className='aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30'
-                        style={{
-                           clipPath:
-                              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
-                        }}
-                     />
-                  </div> */}
-
-                  <div className='flex flex-wrap items-center gap-x-4 gap-y-2'>
-                     <p className='text-xl leading-6 text-gray-900'>
-                        <strong className='font-bold mr-12'>Total price :</strong>
-                        <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
-                           <circle cx={1} cy={1} r={1} />
-                        </svg>
-                        <span className='font-semibold'> $ {cartTotal.toFixed(2)}</span>
-                     </p>
-
-                     <input
-                        className='ml-14 flex-none rounded bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900'
-                        type='submit'
-                        value='Confirm Order'
-                        onClick={formHandleSubmit}
-                     />
-                  </div>
+               <div className='block w-full bg-white p-8'>
+                  <input
+                     className='block mx-auto w-48 lg:w-64 rounded bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 active:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900'
+                     type='submit'
+                     value='Confirm Order'
+                     onClick={formHandleSubmit}
+                  />
                </div>
             </div>
          </div>
