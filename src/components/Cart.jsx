b02a30e44bec7d971748e1f0f8f3c8b8b63b5f7e
hustle2live <kuz.v@ukr.net>
2024-02-11 14:16:50 +0200

    moderate cart total section

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -211,15 +211,9 @@ const ShoppingCart = () => {
             </div>
             {/* <div className={styles.cart_wrapper__totalSection}> */}
             <div className='absolute block box-border bottom-0 w-full'>
-               {/* <p className={styles.cart_wrapper__totalSection_totalAmount}>
-                  <span>Total price : </span>
-                  {cartTotal.toFixed(2)}
-               </p> */}
-               {/* <input type='submit' value='Submit' onClick={formHandleSubmit} /> */}
-
                <div className='relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 p-4 px-32 sm:before:flex-1'>
                   {/* blur background */}
-                  <div
+                  {/* <div
                      className='absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
                      aria-hidden='true'
                   >
@@ -230,9 +224,9 @@ const ShoppingCart = () => {
                               'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
                         }}
                      />
-                  </div>
+                  </div> */}
 
-                  <div
+                  {/* <div
                      className='absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
                      aria-hidden='true'
                   >
@@ -243,7 +237,7 @@ const ShoppingCart = () => {
                               'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
                         }}
                      />
-                  </div>
+                  </div> */}
 
                   <div className='flex flex-wrap items-center gap-x-4 gap-y-2'>
                      <p className='text-xl leading-6 text-gray-900'>
